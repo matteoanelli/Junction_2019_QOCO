c9a82cc099cc1be9647a80d2a8a6bb3eb9d7ebcc
DeFloorIsJava <bakoad@gmail.com>
2019-11-16 14:59:12 +0200

    added html

---
+++ message_sender/email_send_test.py
@@ -0,0 +1,11 @@
+
+import smtplib
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+#Next, log in to the server
+server.login("trackerbaggage@gmail.com", "Abcd1234!")
+
+
+#Send the mail
+msg = "BIG CHUNGUS"
+server.sendmail("trackerbaggage@gmail.com", "baggagecustomer@gmail.com", msg)
