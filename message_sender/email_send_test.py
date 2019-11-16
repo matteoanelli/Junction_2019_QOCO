14293d324e391e13ec8d802cbbbe05af4b0181a6
damianodallapiccola <damiano.dallapiccola@gmail.com>
2019-11-16 15:45:47 +0200

    created email sender

--- message_sender/email_send_test.py
+++ message_sender/email_send_test.py
@@ -1,11 +1,25 @@
+from email.mime.text import MIMEText
 import smtplib
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-#Next, log in to the server
-server.login("trackerbaggage@gmail.com", "Abcd1234!")
+
+sender_email = "trackerbaggage@gmail.com"
+receiver_email = "baggagecustomer@gmail.com"
+password = "Abcd1234!" #input("Type your password and press enter:")
 
 
-#Send the mail
-msg = "BIG CHUNGUS"
-server.sendmail("trackerbaggage@gmail.com", "baggagecustomer@gmail.com", msg)
+html = open("message.html")
+msg = MIMEText(html.read(), 'html')
+msg['From'] = sender_email
+msg['To'] = receiver_email
+msg['Subject'] = "Automatic Weekly Report"
+
+debug = False
+if debug:
+    print(msg.as_string())
+else:
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.starttls()
+    server.login(sender_email, password)
+    text = msg.as_string()
+    server.sendmail(sender_email, receiver_email, text)
+    server.quit()
